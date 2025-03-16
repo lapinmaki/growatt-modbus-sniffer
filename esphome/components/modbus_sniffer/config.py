e2d0bb1b883087725b7358ce8cd76e44a173d9c6
lapinmaki <140404416+lapinmaki@users.noreply.github.com>
2025-03-16 20:46:57 +0200

    Update config.py

--- esphome/components/modbus_sniffer/config.py
+++ esphome/components/modbus_sniffer/config.py
@@ -4,23 +4,17 @@ from esphome.const import CONF_ID
 
 import esphome.codegen as cg
 
-# Define custom constants
 CONF_TEXT_SENSOR = "text_sensor"
 
-# Create a namespace for modbus_sniffer
 modbus_sniffer_ns = cg.esphome_ns.namespace("modbus_sniffer")
-
-# Define ModbusSniffer class
 ModbusSniffer = modbus_sniffer_ns.class_("ModbusSniffer", cg.Component, uart.UARTDevice)
 
-# Define the schema for the modbus_sniffer component
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ModbusSniffer),
     cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
     cv.Required(CONF_TEXT_SENSOR): cv.use_id(text_sensor.TextSensor),
 }).extend(cv.COMPONENT_SCHEMA)
 
-# Function to define the logic for the component
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID], config["uart_id"])
     await cg.register_component(var, config)
